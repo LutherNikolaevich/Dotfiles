b33659fe396c7a539fdd63ebf20d64b338815e8f
Luther <90941318+LutherNikolaevich@users.noreply.github.com>
2026-06-28 01:25:45 +0000

    v1.1

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -182,6 +182,7 @@
     fastfetch
     android-tools
     nodejs
+    localsend
   ];
 
   # Garbage collector
@@ -234,10 +235,26 @@
   services.lact.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ { from = 1714; to = 1764; } ];
-  # networking.firewall.allowedUDPPorts = [ { from = 1714; to = 1764; } ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  networking.firewall = 
+  {
+    enable = true;
+    allowedTCPPorts = 
+    [
+      1714 1715 1716 1717 1718 1719 1720 1721 1722 1723 1724
+      1725 1726 1727 1728 1729 1730 1731 1732 1733 1734 1735
+      1736 1737 1738 1739 1740 1741 1742 1743 1744 1745 1746
+      1747 1748 1749 1750 1751 1752 1753 1754 1755 1756 1757
+      1758 1759 1760 1761 1762 1763 1764
+    ];
+    allowedUDPPorts = 
+    [
+      1714 1715 1716 1717 1718 1719 1720 1721 1722 1723 1724
+      1725 1726 1727 1728 1729 1730 1731 1732 1733 1734 1735
+      1736 1737 1738 1739 1740 1741 1742 1743 1744 1745 1746
+      1747 1748 1749 1750 1751 1752 1753 1754 1755 1756 1757
+      1758 1759 1760 1761 1762 1763 1764
+    ];
+  };
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
