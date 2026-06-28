86d7492b8e141983a89ca2b795ed77daf19cdd40
Luther <90941318+LutherNikolaevich@users.noreply.github.com>
2026-06-28 01:15:30 +0000

    Added nixos

---
+++ etc/nixos/configuration.nix
@@ -0,0 +1,253 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+  [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # AMD GPU
+  hardware.graphics = 
+  {
+    enable = true;
+    extraPackages = with pkgs; [ rocmPackages.clr.icd ];
+  };
+  hardware.amdgpu.opencl.enable = true;
+  environment.variables = 
+  {
+    ROC_ENABLE_PRE_VEGA = "1";
+  };
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Hostname
+  networking.hostName = "nixos";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Manila";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = 
+  {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # Gnome
+  environment.gnome.excludePackages = with pkgs;
+  [
+    gnome-tour
+    gnome-weather
+    gnome-contacts
+    gnome-maps
+    gnome-connections
+    gnome-logs
+    gnome-music
+    gnome-console
+    gnome-user-docs
+    gnome-software
+    epiphany
+    evince
+    simple-scan
+    snapshot
+    seahorse
+    yelp
+    decibels
+    showtime
+  ];
+
+  # Debloat
+  services.xserver.excludePackages = [ pkgs.xterm ];
+
+  # Configure keymap in X11
+  services.xserver.xkb = 
+  {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = false;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = 
+  {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users."czeux" = 
+  {
+    isNormalUser = true;
+    description = "Lhord Czedrick";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; 
+    [
+    #  thunderbird
+    ];
+  };
+
+  # Zsh
+  programs.zsh = 
+  {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    histSize = 10000;
+    histFile = "$HOME/.zsh_history";
+    setOptions = 
+    [
+      "HIST_IGNORE_ALL_DUPS"
+    ];
+    promptInit = ''
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+    '';
+    ohMyZsh = 
+    {
+      enable = true;
+      plugins = 
+      [
+        "git"
+      ];
+    };
+  };  
+
+  # Install firefox.
+  programs.firefox.enable = false;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; 
+  [
+    wget
+    neovim
+    brave
+    kitty
+    onlyoffice-desktopeditors
+    uget
+    songrec
+    mission-center
+    lact
+    warehouse
+    vscode
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.alphabetical-app-grid
+    gnomeExtensions.clipboard-indicator
+    gnomeExtensions.gsconnect
+    opencode
+    git
+    curl
+    zsh-powerlevel10k
+    fastfetch
+    android-tools
+    nodejs
+  ];
+
+  # Garbage collector
+  nix.gc = 
+  {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+  nix.optimise.automatic = true;
+
+  # Zram
+  zramSwap = 
+  {
+    enable = true;
+    priority = 100;
+    algorithm = "lz4";
+    memoryPercent = 50;
+  };
+
+  # Udev Rules
+  services.udev.extraRules = 
+  ''
+    # MCHOSE Jet75-II Keyboard (Vendor: 41e4, Product: 211a)
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="41e4", ATTRS{idProduct}=="211a", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+    # CX Wireless Mouse (Vendor: 373b, Product: 1085)
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="373b", ATTRS{idProduct}=="1085", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+  '';
+
+  # Fonts
+  fonts.packages = with pkgs;
+  [
+    nerd-fonts.jetbrains-mono
+    corefonts
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+  services.flatpak.enable = true;
+  services.lact.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ { from = 1714; to = 1764; } ];
+  # networking.firewall.allowedUDPPorts = [ { from = 1714; to = 1764; } ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "26.05"; # Did you read the comment?
+}
